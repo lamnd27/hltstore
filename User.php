7ae91c2259d0f56a7fa1b49299af5a944e2d7690
lamnd27 <106128147+lamnd27@users.noreply.github.com>
2023-12-13 00:09:56 +0700

    Create User Entity

---
+++ User.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $hometown = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $phonenum = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $state = null;
+
+    #[ORM\OneToMany(mappedBy: 'cuserid', targetEntity: Cart::class)]
+    private Collection $carts;
+
+    public function __construct()
+    {
+        $this->carts = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getHometown(): ?string
+    {
+        return $this->hometown;
+    }
+
+    public function setHometown(string $hometown): static
+    {
+        $this->hometown = $hometown;
+
+        return $this;
+    }
+
+    public function getPhonenum(): ?string
+    {
+        return $this->phonenum;
+    }
+
+    public function setPhonenum(string $phonenum): static
+    {
+        $this->phonenum = $phonenum;
+
+        return $this;
+    }
+
+    public function getState(): ?int
+    {
+        return $this->state;
+    }
+
+    public function setState(int $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Cart>
+     */
+    public function getCarts(): Collection
+    {
+        return $this->carts;
+    }
+
+    public function addCart(Cart $cart): static
+    {
+        if (!$this->carts->contains($cart)) {
+            $this->carts->add($cart);
+            $cart->setCuserid($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCart(Cart $cart): static
+    {
+        if ($this->carts->removeElement($cart)) {
+            // set the owning side to null (unless already changed)
+            if ($cart->getCuserid() === $this) {
+                $cart->setCuserid(null);
+            }
+        }
+
+        return $this;
+    }
+}
