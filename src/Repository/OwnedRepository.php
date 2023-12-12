9595af79c510b3d69f4c665be6e8331938a331ed
QuochuygrwCT <huylqgbc210300@fpt.edu.vn>
2023-12-12 23:46:43 +0700

    create owned  Repository

---
+++ src/Repository/OwnedRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Owned;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Owned>
+ *
+ * @method Owned|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Owned|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Owned[]    findAll()
+ * @method Owned[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class OwnedRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Owned::class);
+    }
+
+//    /**
+//     * @return Owned[] Returns an array of Owned objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->andWhere('o.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('o.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Owned
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->andWhere('o.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
